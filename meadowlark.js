65d340d87ac095abf1e73ab3b36a44f1c030fe3d
Bryan Leonard <b.leonard@zoho.com>
2015-02-05 16:16:54 -0500

    More things

--- meadowlark.js
+++ meadowlark.js
@@ -34,6 +34,15 @@ app.use(function(req, res, next) {
 	next();
 });
 
+/**
+ * Request headers test route
+ */
+app.get('/headers', function(req,res){
+	res.set('Content-Type','text/plain');
+	var s = '';
+	for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+	res.send(s);
+});
 
 
 /**
